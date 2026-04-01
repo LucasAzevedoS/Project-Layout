52316cb0a71e1ae7e2c99f556da5aac4406e6890
Lucas Azevedo <lucas.a.souza@hotmail.com>
2026-04-01 17:44:03 -0300

    refactor: use dependency injection in UsuarioRepository

---
+++ ApiWeb/Repositories/UsuarioRepository.cs
@@ -0,0 +1,52 @@
+﻿using ApiWeb.Services;
+using ApiWeb.Models;
+using ApiWeb.Services.InputModels;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace ApiWeb.Repositories;
+
+public class UsuarioRepository
+{
+    private readonly UtilService _utilService;
+
+    public UsuarioRepository(UtilService utilService)
+    {
+        _utilService = utilService;
+    }
+
+    public int? IncluiUsuario(IncluiUsuarioInputModel usuario)
+    {
+        string query = @"exec stp_incluiUsuario 
+             @KeycloakId = @KeycloakId,
+             @Nome = @Nome,
+             @Email = @Email,
+             @DtCriacao = @DtCriacao";
+
+        using (var conn = new SqlConnection(_utilService.SeviceConn()))
+        {
+            conn.Open();
+            return conn.Query<int>(query, new
+            {
+                DtCriacao = usuario.DtCriacao,
+                Nome = usuario.Nome,
+                Email = usuario.Email,
+                KeycloakId = usuario.KeycloakId
+            }).FirstOrDefault();
+        }
+    }
+
+    public Usuario? BuscarPorKeycloakId(string keycloakId)
+    {
+        const string query = "exec stp_buscaUsuarioPorKeycloakId @KeycloakId";
+
+        using (var conn = new SqlConnection(_utilService.SeviceConn()))
+        {
+            conn.Open();
+            return conn.QueryFirstOrDefault<Usuario>(query, new
+            {
+                KeycloakId = keycloakId
+            });
+        }
+    }
+}

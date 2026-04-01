83a4df6573f930cb132012e618f5e27b82d6945e
Lucas Azevedo <lucas.a.souza@hotmail.com>
2026-04-01 17:43:58 -0300

    refactor: replace manual JSON reading with IConfiguration injection

---
+++ ApiWeb/Services/UtilService.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+namespace ApiWeb.Services;
+
+public class UtilService
+{
+    private readonly IConfiguration _configuration;
+
+    public UtilService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string? SeviceConn()
+    {
+        var ambiente = _configuration["Ambiente"];
+        var conn = ambiente == "P"
+            ? _configuration["ServiceConn_P"]
+            : _configuration["ServiceConn_H"];
+
+        Console.WriteLine($"Ambiente: {ambiente}");
+        Console.WriteLine($"ConnectionString: {conn}");
+
+        return conn;
+    }
+}

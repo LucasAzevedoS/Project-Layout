277304fa075ef185fb202c5c7e4535b147df05fe
Lucas Azevedo <lucas.a.souza@hotmail.com>
2026-04-01 17:45:13 -0300

    chore: Model of Auth user

---
+++ ApiWeb/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiWeb.Models;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    public string KeycloakId { get; set; }
+    public DateTime DtCriacao { get; set; }
+    public string? Nome { get; set; }
+    public string? Email { get; set; }
+}
---
+++ ApiWeb/Services/InputModels/IncluiUsuarioInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiWeb.Services.InputModels;
+
+public class IncluiUsuarioInputModel
+{
+    public DateTime DtCriacao { get; set; } = DateTime.Now;
+    public string? Nome {get;set;}
+    public string? Email {get;set;}
+    public string KeycloakId { get; set; }
+}

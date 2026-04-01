70598f2087d2ed466e8e2f20f0cc46dda75c3800
Lucas Azevedo <lucas.a.souza@hotmail.com>
2026-04-01 17:44:06 -0300

    refactor: use dependency injection in UsuarioService

---
+++ ApiWeb/Services/UsuarioService.cs
@@ -0,0 +1,25 @@
+﻿using ApiWeb.Models;
+using ApiWeb.Repositories;
+using ApiWeb.Services.InputModels;
+
+namespace ApiWeb.Services;
+
+public class UsuarioService
+{
+    private readonly UsuarioRepository _usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository)
+    {
+        _usuarioRepository = usuarioRepository;
+    }
+
+    public int? IncluirUsuario(IncluiUsuarioInputModel usuario)
+    {
+        return _usuarioRepository.IncluiUsuario(usuario);
+    }
+
+    public Usuario? BuscarPorKeycloakId(string keycloakId)
+    {
+        return _usuarioRepository.BuscarPorKeycloakId(keycloakId);
+    }
+}
